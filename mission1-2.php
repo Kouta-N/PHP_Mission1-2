7a69fa56f07bb81668d5eea923e3acb441822551
kakashi <nk29time@gmail.com>
2021-06-02 21:33:45 +0900

    first

--- mission1-2.php
+++ mission1-2.php
@@ -10,7 +10,18 @@
 </head>
 
 <body>
-
+  <form method="POST" action="mission1-2.php">
+    <input type="text" name="search" value="" />
+    <input type="submit" name="send" value="検索" />
+  </form>
+  <?php
+  $fruits = ['apple', 'orange', 'strawberry'];
+if(!empty($_POST['send']) && array_search($_POST['search'],$fruits,true) !== false){
+  echo $_POST['search'].'は,配列fruitsの中に存在しています。';
+}else if(!empty($_POST['send']) ){
+  echo $_POST['search'].'は,配列fruitsの中に存在していません。';
+}
+?>
 </body>
 
 </html>
